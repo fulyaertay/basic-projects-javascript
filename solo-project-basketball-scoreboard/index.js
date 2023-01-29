cda5f2166e8315a937c0c3061480a2861e600ccb
Fulya <fulya.ertay@gmail.com>
2023-01-29 16:18:58 +0300

    added highlight for winner team

--- solo-project-basketball-scoreboard/index.js
+++ solo-project-basketball-scoreboard/index.js
@@ -12,20 +12,42 @@ let guestCounter=document.getElementById("guest-counter")
 let increaseHome=0;
 let increaseGuest=0;
 
+let guestContent=document.getElementsByClassName("guest-content")
 //increase home counter
 function homeAdd1(){
     increaseHome+=1;
     homeCounter.textContent= increaseHome;
+    if (increaseHome > increaseGuest){
+        homeCounter.style.color=" green";
+        guestCounter.style.color="#F94F6D";
+    }
+    else{
+        homeCounter.style.color="#F94F6D";
+    }
     
 }
 function homeAdd2(){
     increaseHome+=2;
     homeCounter.textContent= increaseHome;
+    if (increaseHome > increaseGuest){
+        homeCounter.style.color=" green";
+        guestCounter.style.color="#F94F6D";
+    }
+    else{
+        homeCounter.style.color="#F94F6D";
+    }
     
 }
 function homeAdd3(){
     increaseHome+=3;
     homeCounter.textContent= increaseHome;
+    if (increaseHome > increaseGuest){
+        homeCounter.style.color=" green";
+        guestCounter.style.color="#F94F6D";
+    }
+    else{
+        homeCounter.style.color="#F94F6D";
+    }
     
 }
 
@@ -33,15 +55,47 @@ function homeAdd3(){
 function guestAdd1(){
     increaseGuest+=1;
     guestCounter.textContent=  increaseGuest;
+    if (increaseGuest > increaseHome){
+        guestCounter.style.color=" green";
+        homeCounter.style.color="#F94F6D";
+    }
+    else{
+        guestCounter.style.color="#F94F6D";
+    }
     
 }
 function guestAdd2(){
     increaseGuest+=2;
     guestCounter.textContent=  increaseGuest;
+    if (increaseGuest > increaseHome){
+        guestCounter.style.color=" green";
+        homeCounter.style.color="#F94F6D";
+    }    else{
+        guestCounter.style.color="#F94F6D";
+    }
+    
     
 }
 function guestAdd3(){
     increaseGuest+=3;
     guestCounter.textContent=  increaseGuest;
+    if (increaseGuest > increaseHome){
+        guestCounter.style.color=" green";
+        homeCounter.style.color="#F94F6D";
+    }    else{
+        guestCounter.style.color="#F94F6D";
+    }
     
+    
+}
+
+let newButton=document.getElementsByClassName("newButton")
+//reset when click new game button
+function newGame(){
+    increaseHome=0;
+    increaseGuest=0;
+    homeCounter.textContent=increaseHome;
+    guestCounter.textContent=increaseGuest;
+
+
 }
