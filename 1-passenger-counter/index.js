4a3f62b8062d29b273ba359f17884e7fa344fb83
Fulya <fulya.ertay@gmail.com>
2023-01-29 14:07:57 +0300

    feat:edited count 0 when clicked save button

--- 1-passenger-counter/index.js
+++ 1-passenger-counter/index.js
@@ -1,22 +1,16 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-console.log(saveEl)
-
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText += countStr
-    // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
-
 
