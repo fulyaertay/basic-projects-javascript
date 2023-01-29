c31c2fb1f078c85935d8e7e21a158fd5e6da3a67
Fulya <fulya.ertay@gmail.com>
2023-01-29 13:32:39 +0300

    added save button

--- 1-passenger-counter/index.js
+++ 1-passenger-counter/index.js
@@ -1,15 +1,22 @@
-// document.getElementById("count-el").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
-let countEl = document.getElementById("count-el") // pass in arguments
-
-console.log(countEl)
-
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
+console.log(saveEl)
+
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
+
+function save() {
+    let countStr = count + " - "
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText += countStr
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+}
+
+
