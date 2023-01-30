a53bc7c179bd8fbdc1e36b6d1320757bc8432b8c
Fulya <fulya.ertay@gmail.com>
2023-01-30 14:35:28 +0300

    added sum of cards

--- 3-black-jack-app/index.js
+++ 3-black-jack-app/index.js
@@ -1,15 +1,16 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
-// 1. Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+// 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+    // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -19,6 +20,5 @@ function startGame() {
         message = "You're out of the game!"
         isAlive = false
     }
-    // 2. Display the message in the messageEl using messageEl.textContent
     messageEl.textContent = message
 }
