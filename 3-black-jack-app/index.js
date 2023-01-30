dae0d8cab03296b9cd0a481b61ef74f9cbe23c11
Fulya <fulya.ertay@gmail.com>
2023-01-30 14:21:02 +0300

    edited button style

--- 3-black-jack-app/index.js
+++ 3-black-jack-app/index.js
@@ -3,19 +3,20 @@ let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
 let message = ""
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
 
-// 3. Log it out!
-console.log(message)
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)    
+}
