45d56a9ba282972cd763fa036d358be50bc8bdad
Fulya <fulya.ertay@gmail.com>
2023-01-30 13:53:28 +0300

    feat:init blackJack app

---
+++ 3-blackJack-app/index.js
@@ -0,0 +1,18 @@
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard + 4
+
+// Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+// else -> "You're out of the game! 😭"
+
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
