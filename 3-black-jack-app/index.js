a986490eb5db7fc9edc206a7fdae0ef3cf60bcce
Fulya <fulya.ertay@gmail.com>
2023-01-30 14:29:42 +0300

    added message

--- 3-black-jack-app/index.js
+++ 3-black-jack-app/index.js
@@ -1,22 +1,24 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)    
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
 }
