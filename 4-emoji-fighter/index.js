de0e6212bad4749a2133d0adec3a66cbb213036a
Fulya <fulya.ertay@gmail.com>
2023-01-30 21:11:31 +0300

    renamed variables

--- 4-emoji-fighter/index.js
+++ 4-emoji-fighter/index.js
@@ -6,7 +6,7 @@ let fighters = [
   "🦍",
   "🐢",
   "🐩",
-  "aa",
+  "🐳",
   "🦀",
   "🐝",
   "🤖",
@@ -20,17 +20,17 @@ let fighters = [
 
 let stageEl = document.getElementById("stage");
 let fightButton = document.getElementById("fightButton");
-let getemj1 = 0;
-let getemj2 = 0;
+let getEmj1 = 0;
+let getEmj2 = 0;
 fightButton.addEventListener("click", function () {
   for (let emoji = 1; emoji < fighters.length + 1; emoji++) {
     let first = Math.floor(Math.random() * fighters.length);
-    getemj1 = fighters[first];
+    getEmj1 = fighters[first];
     let second = Math.floor(Math.random() * fighters.length);
-    getemj2 = fighters[second];
+    getEmj2 = fighters[second];
   }
 
-  stageEl.textContent = getemj1 + "vs" + getemj2;
+  stageEl.textContent = getEmj1 + "vs" + getEmj2;
 
   // Challenge:
   // When the user clicks on the "Pick Fighters" button, pick two random
