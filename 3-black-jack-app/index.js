a16992315fe820ed3ecad6f43560fe178a028073
Fulya <fulya.ertay@gmail.com>
2023-01-30 15:27:23 +0300

    added card in array

--- 3-black-jack-app/index.js
+++ 3-black-jack-app/index.js
@@ -1,6 +1,6 @@
 let firstCard = 10
 let secondCard = 4
-let cards = [firstCard, secondCard] // array - ordered list of items
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -14,9 +14,7 @@ function startGame() {
 }
 
 function renderGame() {
-    // render out firstCard and secondCard
     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    // render out ALL the cards we have
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -34,5 +32,8 @@ function renderGame() {
 function newCard() {
     let card = 6
     sum += card
+    // Push the card to the cards array
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
