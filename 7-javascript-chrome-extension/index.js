ee62d9c3c4ab2d4daf71ebfeba2a7df8fd6712c5
Fulya <fulya.ertay@gmail.com>
2023-01-31 14:51:59 +0300

    added event function

--- 7-javascript-chrome-extension/index.js
+++ 7-javascript-chrome-extension/index.js
@@ -1,6 +1,10 @@
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+    console.log("Button clicked!")
 })
+
 
