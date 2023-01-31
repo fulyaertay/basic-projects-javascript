46c7cd912966049dfefcb032d54d61606d8b4bb4
Fulya <fulya.ertay@gmail.com>
2023-01-31 15:51:01 +0300

    rendered listItems as link

--- 7-javascript-chrome-extension/index.js
+++ 7-javascript-chrome-extension/index.js
@@ -8,10 +8,15 @@ inputBtn.addEventListener("click", function() {
     console.log(myLeads)
 })
 
-// Replace .textContent with .innerHTML and use <li> tags
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+    listItems += "<li>" + myLeads[i] + "</li>"
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems
 
 
 
