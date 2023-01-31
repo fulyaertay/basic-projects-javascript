29c50fe1964d62ad8777c2e8a4612cd00314758a
Fulya <fulya.ertay@gmail.com>
2023-01-31 21:03:24 +0300

    saved tab button value

--- 7-javascript-chrome-extension/index.js
+++ 7-javascript-chrome-extension/index.js
@@ -4,7 +4,6 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-// 1. Grab the SAVE TAB button and store it in a tabBtn variable
 const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
@@ -16,9 +15,12 @@ const tabs = [
     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
 ]
 
-// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
 tabBtn.addEventListener("click", function(){
-    console.log(tabs[0].url)
+    // Save the url instead of logging it out
+    // console.log(tabs[0].url)
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
 
 function render(leads) {
