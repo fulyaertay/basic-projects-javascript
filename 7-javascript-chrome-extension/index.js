07024d19adb4b9c7fce8d157b0d9fcc6c803f112
Fulya <fulya.ertay@gmail.com>
2023-01-31 20:55:24 +0300

    feat:refactored index.js

--- 7-javascript-chrome-extension/index.js
+++ 7-javascript-chrome-extension/index.js
@@ -1,5 +1,4 @@
 let myLeads = []
-let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -10,6 +9,10 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+// Refector the function so that it takes a parameter, leads, that it uses
+// instead of the global myLeads variable. Remember to update all invocations 
+// of the function as well.
 
 function render(leads) {
     let listItems = ""
