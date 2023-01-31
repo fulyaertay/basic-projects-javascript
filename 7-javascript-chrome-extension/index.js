e111f8432d26de3193f190fefbbe4b15d9dba0ae
Fulya <fulya.ertay@gmail.com>
2023-01-31 14:54:59 +0300

    created arrays for inputs

--- 7-javascript-chrome-extension/index.js
+++ 7-javascript-chrome-extension/index.js
@@ -1,7 +1,9 @@
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
-
-let inputBtn = document.getElementById("input-btn")
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
