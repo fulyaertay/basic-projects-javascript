6d0e6305ab9fcb869e2648a918311643d8f5e9ab
Fulya <fulya.ertay@gmail.com>
2023-02-01 19:29:49 +0300

    added function

--- 8-solo-project-unit-converter/index.js
+++ 8-solo-project-unit-converter/index.js
@@ -13,30 +13,34 @@ let kipo=document.getElementById("kipo")
 
 let getInputVal=document.getElementById("inputText")
 
-
-convertBtn.addEventListener("click",function(){
+function unitConverter(){
+    convertBtn.addEventListener("click",function(){
     
-    if (getInputVal.value>0){
-        let convertedVal=Number(getInputVal.value)
+        if (getInputVal.value>0){
+            let convertedVal=Number(getInputVal.value)
+            
+            //convert meters vs feet
+            metfet.textContent=`
+            ${convertedVal} meters = ${(convertedVal*3.281).toFixed(3)} feet | 
+            ${convertedVal} feet = ${(convertedVal*0.304).toFixed(3)} meters `
         
-        //convert meters vs feet
-        metfet.textContent=`
-        ${convertedVal} meters = ${(convertedVal*3.281).toFixed(3)} feet | 
-        ${convertedVal} feet = ${(convertedVal*0.304).toFixed(3)} meters `
+            //convert liters vs gallons
+            liga.textContent=`
+            ${convertedVal} liters = ${(convertedVal*0.264).toFixed(3)} gallons | 
+            ${convertedVal} gallons = ${(convertedVal*3.785).toFixed(3)} liters `
+        
+            //convert kilos vs pounds
+            kipo.textContent=`
+            ${convertedVal} kilos = ${(convertedVal*2.204).toFixed(3)} pounds | 
+            ${convertedVal} pounds = ${(convertedVal*0.480 ).toFixed(3)} kilos `
+        
+        }else{
+            alert("You must enter a number to convert!")
+        }
+       
     
-        //convert liters vs gallons
-        liga.textContent=`
-        ${convertedVal} liters = ${(convertedVal*0.264).toFixed(3)} gallons | 
-        ${convertedVal} gallons = ${(convertedVal*3.785).toFixed(3)} liters `
-    
-        //convert kilos vs pounds
-        kipo.textContent=`
-        ${convertedVal} kilos = ${(convertedVal*2.204).toFixed(3)} pounds | 
-        ${convertedVal} pounds = ${(convertedVal*0.480 ).toFixed(3)} kilos `
-    
-    }else{
-        alert("You must enter a number to convert!")
-    }
-   
+    })
 
-})
+}
+
+unitConverter()
