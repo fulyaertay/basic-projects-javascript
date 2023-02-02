4773d1418a609c808b74ab9ccc150cbd21fa65d9
Fulya <fulya.ertay@gmail.com>
2023-02-02 20:00:27 +0300

    added emojis in container

--- 9-solo-project-my-emojis/index.js
+++ 9-solo-project-my-emojis/index.js
@@ -1,7 +1,10 @@
-// 1. Loop through the myEmojis array and log each of them out to the console
+// Grab the emojiContainer div and store it in a variable. Log it to the console
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emojiContainer")
+
+console.log(emojiContainer)
 
 for (let i = 0; i < myEmojis.length; i++) {
-    console.log(myEmojis[i])
+    // console.log(myEmojis[i])
 }
